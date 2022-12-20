a2ce06f75256542a7ba114f4945e07e8560c8c5f
GoPieRRe <98568059+GoPieRRe@users.noreply.github.com>
2022-12-20 23:00:08 +0700

    multi auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,6 +38,24 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                                {{-- <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"> --}}
+                                <select name="role" id="" class="form-select">
+                                    <option>PILIH</option>
+                                    <option value="bumdes">Bumdes</option>
+                                    <option value="banksampah">BankSampah</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
